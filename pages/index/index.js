eb685bb1c7d5ede58a061c4a94b9ce0c3c96b82b
王兴雨 <ames.yueqian@hotmail.com>
2024-04-16 17:24:47 +0800

    feat: init project

---
+++ pages/index/index.js
@@ -0,0 +1,209 @@
+export const cpuList = [
+  {
+    name: "i9-14900K",
+    type: "cpu",
+    brand: "Intel",
+    generation: "14",
+    level: "i9",
+    coresNumber: 4,
+    threadNumber: 8,
+  },
+  {
+    name: "i7-14700K",
+    type: "cpu",
+    brand: "Intel",
+    generation: "14",
+    level: "i7",
+    coresNumber: 4,
+    threadNumber: 8,
+  },
+  {
+    name: "i5-14600K",
+    type: "cpu",
+    brand: "Intel",
+    generation: "14",
+    level: "i5",
+    coresNumber: 4,
+    threadNumber: 8,
+  },
+];
+
+export const boardList = [
+  {
+    name: "B650M-PLUS TUF GAMING",
+    type: "motherBoard",
+    cpuType: "Intel",
+    memoryType: "DDR4",
+    brandZh: "华硕",
+    brandEn: "ASUS",
+    generation: "14",
+    level: "i9",
+    isWifi: "1",
+  },
+  {
+    name: "H610M-E PRO",
+    type: "motherBoard",
+    cpuType: "Intel",
+    memoryType: "DDR4",
+    brandZh: "微星",
+    brandEn: "MSI",
+    generation: "14",
+    level: "i7",
+    isWifi: "1",
+  },
+  {
+    name: "B760M GAMING AC",
+    type: "motherBoard",
+    cpuType: "Intel",
+    memoryType: "DDR4",
+    brandZh: "技嘉",
+    brandEn: "GIGABYTE",
+    generation: "14",
+    level: "i5",
+    isWifi: "1",
+  },
+];
+
+export const memoryList = [
+  {
+    name: "战斧系列",
+    type: "memory",
+    memoryType: "DDR4",
+    size: "16",
+    memoryFrequency: "2400",
+    brandZh: "三星",
+    brandEn: "ASUS",
+    level: "i9",
+  },
+  {
+    name: "战斧系列",
+    type: "memory",
+    memoryType: "DDR4",
+    size: "16",
+    memoryFrequency: "2400",
+    brandZh: "海力士",
+    brandEn: "MSI",
+    level: "i7",
+  },
+  {
+    name: "战斧系列",
+    type: "memory",
+    memoryType: "DDR4",
+    size: "16",
+    memoryFrequency: "2400",
+    brandZh: "金士顿",
+    brandEn: "GIGABYTE",
+    level: "i5",
+  },
+];
+
+export const gpuList = [
+  {
+    name: "RTX4090",
+    type: "gpu",
+    gpuType: "Nvidia",
+    size: "16",
+    gpuFrequency: "2400",
+    brandZh: "七彩虹",
+    brandEn: "ASUS",
+    level: "i9",
+  },
+  {
+    name: "RTX4090",
+    type: "gpu",
+    gpuType: "AMD",
+    size: "16",
+    gpuFrequency: "2400",
+    brandZh: "华硕",
+    brandEn: "MSI",
+    level: "i7",
+  },
+  {
+    name: "RTX4090",
+    type: "gpu",
+    gpuType: "Nvidia",
+    size: "16",
+    gpuFrequency: "2400",
+    brandZh: "技嘉",
+    brandEn: "GIGABYTE",
+    level: "i5",
+  },
+];
+
+export const ssdList = [
+  {
+    name: "evo",
+    type: "ssd",
+    memoryType: "DDR4",
+    size: "16",
+    brandZh: "三星",
+    brandEn: "ASUS",
+    interface: "i9",
+  },
+  {
+    name: "战斧系列",
+    type: "ssd",
+    memoryType: "DDR4",
+    size: "16",
+    brandZh: "希捷",
+    brandEn: "MSI",
+    interface: "i7",
+  },
+  {
+    name: "SN-570",
+    type: "ssd",
+    memoryType: "DDR4",
+    size: "16",
+    brandZh: "西部数据",
+    brandEn: "GIGABYTE",
+    interface: "i5",
+  },
+];
+
+export const hddList = [
+  {
+    name: "酷狼",
+    type: "hdd",
+    hddType: "DDR4",
+    size: "16",
+    brandZh: "希捷",
+    brandEn: "ASUS",
+  },
+  {
+    name: "蓝盘",
+    type: "hdd",
+    hddType: "DDR4",
+    size: "16",
+    brandZh: "西部数据",
+    brandEn: "MSI",
+  },
+  {
+    name: "战斧系列",
+    type: "hdd",
+    hddType: "DDR4",
+    size: "16",
+    brandZh: "金士顿",
+    brandEn: "GIGABYTE",
+  },
+];
+
+export const powerList = [
+  {
+    name: "战斧系列",
+    type: "power",
+    brandZh: "航嘉",
+    brandEn: "ASUS",
+  },
+  {
+    name: "战斧系列",
+    type: "power",
+    brandZh: "海力士",
+    brandEn: "MSI",
+  },
+  {
+    name: "战斧系列",
+    type: "power",
+    brandZh: "金士顿",
+    brandEn: "GIGABYTE",
+  },
+];
---
+++ pages/index/index.vue
@@ -0,0 +1,140 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
+		<van-sidebar v-model="active" @change="onChange">
+			<van-sidebar-item title="CPU" />
+			<van-sidebar-item title="主板" />
+			<van-sidebar-item title="内存" />
+			<van-sidebar-item title="显卡" />
+			<van-sidebar-item title="固体硬盘" />
+			<van-sidebar-item title="机械硬盘" />
+			<van-sidebar-item title="电源" />
+			<van-sidebar-item title="机箱" />
+		</van-sidebar>
+		<view class="list-area">
+			<view v-for="item in list" style="width: 100%;border-bottom: 1px solid #ccc;padding: 5px 10px;display: flex;">
+				<image class="logo" :src="setImg(item.type)"></image>
+				<view>
+					<view style="display: flex;">
+						<view style="display: flex;margin-left: 5px;">
+							<view v-if="item.brand">{{ item.brand }}</view>
+							<view v-if="item.brandZh">{{ item.brandZh }}</view>
+							<view style="margin-left: 5px;    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-all;
+    white-space: nowrap;">{{ item.name }}</view>
+						</view>
+					</view>
+					<view v-if="item.type == 'cpu'" style="margin-left: 5px;">
+						<van-tag v-if="item.coresNumber" plain type="primary">{{ item.coresNumber }}核</van-tag>
+						<van-tag v-if="item.threadNumber" plain type="primary" style="margin-left: 5px;">{{ item.threadNumber
+							}}线程</van-tag>
+					</view>
+					<view v-if="item.type == 'motherBoard'" style="margin-left: 5px;">
+						<van-tag plain type="primary">{{ item.cpuType }}</van-tag>
+						<van-tag plain type="primary" style="margin-left: 5px;">{{ item.memoryType }}</van-tag>
+					</view>
+					<view v-if="item.type == 'memory'" style="margin-left: 5px;">
+						<van-tag plain type="primary">{{ item.size }}G</van-tag>
+					</view>
+					<view v-if="item.type == 'gpu'" style="margin-left: 5px;">
+						<van-tag plain type="primary">{{ item.size }}G</van-tag>
+					</view>
+					<view v-if="item.type == 'ssd'" style="margin-left: 5px;">
+						<van-tag plain type="primary">{{ item.size }}G</van-tag>
+					</view>
+					<view v-if="item.type == 'hdd'" style="margin-left: 5px;">
+						<van-tag plain type="primary">{{ item.size }}G</van-tag>
+					</view>
+				</view>
+				<!-- <text class="title">{{ title }}</text> -->
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+import {
+	cpuList, boardList, memoryList, gpuList, ssdList, hddList, powerList
+} from './index.js'
+export default {
+	data() {
+		return {
+			title: 'Hello',
+			active: 0,
+			cpuList: cpuList,
+			boardList: boardList,
+			memoryList: memoryList,
+			gpuList: gpuList,
+			ssdList: ssdList,
+			hddList: hddList,
+			powerList: powerList,
+			list: []
+		}
+	},
+	onLoad() {
+
+	},
+	created() {
+		this.list = this.cpuList
+	},
+	methods: {
+		onChange(current) {
+			const {
+				detail
+			} = current
+			if (detail === 0) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.cpuList
+			} else if (detail === 1) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.boardList
+			} else if (detail === 2) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.memoryList
+			} else if (detail === 3) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.gpuList
+			} else if (detail === 4) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.ssdList
+			} else if (detail === 5) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.hddList
+			} else if (detail === 6) {
+				console.log("🚀 ~ onChange ~ detail:", detail)
+				this.list = this.powerList
+			}
+		},
+		setImg(type) {
+			return '/static/image/' + type + '.png'
+		}
+	}
+}
+</script>
+
+<style>
+.content {
+	display: flex;
+	/* flex-direction: column; */
+	/* align-items: center; */
+	/* justify-content: center; */
+}
+
+.logo {
+	width: 100rpx;
+	height: 100rpx;
+}
+
+.list-area {
+	width: 100%;
+	display: flex;
+	/* justify-content: center; */
+	flex-direction: column;
+}
+
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
+</style>
